28f2b2880366da9cb56b5a7d2aecb62262eba3a2
Santi <santiago.bel@webfordesigners.com>
2013-02-07 18:06:20 +0100

    new controllers for playing with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 SoaplabMajordomo::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
+
+  resources :users, :only => :index
+
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +56,6 @@ SoaplabMajordomo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
