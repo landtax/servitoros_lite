3c7636a2e236c65f0be609b9492a680ec1129b38
Santi <santiago.bel@webfordesigners.com>
2013-03-21 17:52:03 +0100

    show/hide menu options when user is not signed in. href to real routes

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -4,6 +4,22 @@ describe "layouts/application.html.erb" do
 
   let(:user) { create(:user) }
 
+  describe "menu" do
+    it "shows options when user is signed_in" do
+      view.stub(:user_signed_in?).and_return(true)
+      view.stub(:current_user).and_return(user)
+      render 
+      rendered.should have_selector("ul.nav li", :count => 2)
+    end
+
+    it "does not show options when user is not signed_in" do
+      view.stub(:user_signed_in?).and_return(false)
+      render 
+      rendered.should_not have_selector("ul.nav li")
+    end
+
+  end
+
   describe "devise controls" do
     it "shows signup when not current user" do
       view.stub(:user_signed_in?).and_return(false)
